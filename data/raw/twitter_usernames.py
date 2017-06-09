9f345c73468a8652b3bdcb297441c955288a8a67
Jonathan <jonathan.hilgart@gmail.com>
2017-06-08 18:31:59 -0700

    updated environment

---
+++ data/raw/twitter_usernames.py
@@ -0,0 +1,10 @@
+#! usr/bin/env Python
+
+company_person_tiwtter = {
+'long_term_stock_exchange': ('eric_ries','ericries'),
+'aingel': ('amr_shady','ashady'),
+'reheva_botanicals': ('jake_mendel','ijakemendel'),
+'voyage_auto': ('oliver_cameron','olivercameron'),
+'nuzzel': ('jonathan_abrams','abrams')
+
+}
--- references/data_model.py
+++ references/data_model.py
@@ -1,10 +1,11 @@
 colum_headers = ["""
 month_founded, day_founded,
 
-ceo_sentiment
-ceo_openness_big5, ceo_conscientiousness_big5, ceo_extraversion_big5, ceo_agreeableness_big5,
-ceo_emotional_range_big5, ceo_anger_tone, ceo_disgust_tone, ceo_fear_tone, ceo_joy_tone,
-ceo_sadness_tone,
+founder1_sentiment, founder1_age,
+founder1_openness_big5, founder1_conscientiousness_big5, founder1_extraversion_big5, founder1_agreeableness_big5,
+founder1_emotional_range_big5, founder1_anger_tone, ceo_disgust_tone, founder1_fear_tone, founder1_joy_tone,
+founder1_sadness_tone,
 
+service_tier_gold, service_tier_silver, service_tier_bronze,
 size_of_founding_team, company_niche,
  num_twitter_followers"""]
